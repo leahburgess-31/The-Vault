75afc900dca864dcb9940d6e9f5553aaec041d65
Leah Burgess <l2burges@uwaterloo.ca>
2021-11-26 13:11:53 -0500

    main code

--- src/main.c
+++ src/main.c
@@ -1,30 +1,25 @@
-// Sample code for ECE 198
-
-// Written by Bernie Roehl, August 2021
-
-// This file contains code for a number of different examples.
-// Each one is surrounded by an #ifdef ... #endif block inside of main().
-
-// To run a particular example, you should remove the comment (//) in
-// front of exactly ONE of the following lines:
-
-#define BUTTON_BLINK
-// #define LIGHT_SCHEDULER
-// #define TIME_RAND
-// #define KEYPAD
-// #define KEYPAD_CONTROL
-// #define SEVEN_SEGMENT
-// #define KEYPAD_SEVEN_SEGMENT
-// #define COLOR_LED
-// #define ROTARY_ENCODER
-// #define ANALOG
-// #define PWM
+/**
+ * @brief ECE198 The Vault Project 
+ * @author Kelly Mak:
+    * @passcode setup 
+    * @led lights 
+    * @LCd display 
+    * @timer 
+* @author Leah Burgeess:
+    * @ servo motor 
+    * @4 digit 7 segment display
+    * @joystick 
+    * @buzzer
+ * 
+ */
 
 #include <stdbool.h> // booleans, i.e. true and false
 #include <stdio.h>   // sprintf() function
 #include <stdlib.h>  // srand() and random() functions
 
-#include "ece198.h"
+#include "ece198.h" // import ece198 library
+#include "LiquidCrystal.h" // import LiquidCrystal Library
+
 
 int main(void)
 {
@@ -37,233 +32,153 @@ int main(void)
     __HAL_RCC_GPIOB_CLK_ENABLE(); // enable port B (for the rotary encoder inputs, for example)
     __HAL_RCC_GPIOC_CLK_ENABLE(); // enable port C (for the on-board blue pushbutton, for example)
 
-    // initialize the pins to be input, output, alternate function, etc...
-
-    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // on-board LED
-
-    // note: the on-board pushbutton is fine with the default values (no internal pull-up resistor
-    // is required, since there's one on the board)
-
-    // set up for serial communication to the host computer
-    // (anything we write to the serial port will appear in the terminal (i.e. serial monitor) in VSCode)
-
-    SerialSetup(9600);
-
-    // as mentioned above, only one of the following code sections will be used
-    // (depending on which of the #define statements at the top of this file has been uncommented)
-
-#ifdef BUTTON_BLINK
-    // Wait for the user to push the blue button, then blink the LED.
-
-    // wait for button press (active low)
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
-    {
+    // passcode setup 
+    int passcode [4] = {0,0,0,0};  
+    // get tick for randomizer 
+    bool randInitialized = false;
+    if (!randInitialized){
+        srand(HAL_GetTick());
     }
-
-    while (1) // loop forever, blinking the LED
-    {
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        HAL_Delay(250);  // 250 milliseconds == 1/4 second
+    // get random number for each digit 
+    for (int i = 0; i < 4; i++){
+        passcode[i] = rand()%10;
     }
-#endif
+    // get the 4 digits from array 
+    int result = passcode[0]+passcode[1] *10+passcode[2]*100+passcode[3]*1000;
 
-#ifdef LIGHT_SCHEDULER
-    // Turn on the LED five seconds after reset, and turn it off again five seconds later.
+// conversion from decimal to binary 
+    char b1[20];
+    itoa(passcode[0], b1, 2);
+    char b2 [20];
+    itoa(passcode[1], b2, 2);
+    char b3 [20];
+    itoa(passcode[2], b3, 2);
+    char b4 [20];
+    itoa(passcode[3], b4, 2);
+ 
+ // conversion from binary to character for lcd display 
+    char pass[20];
+    char pass1[20];
+    char pass2[20];
+    char pass3[20];
+    sprintf(pass, "%d", passcode[0]);
+    sprintf(pass1, "%d", passcode[1]);
+    sprintf(pass2, "%d", passcode[2]);
+    sprintf(pass3, "%d", passcode[3]);
 
-    while (true) {
-        uint32_t now = HAL_GetTick();
-        if (now > 5000 && now < 10000)
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, true);   // turn on LED
-        else
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, false);  // turn off LED
-    }
-#endif
+    // Initializing the pinouts for the components
+    Initialize4Digit7Segment(); //4 digits 7 segment display
+    InitializeJoystick(); // joystick 
+    InitializeBuzzer(); //buzzer 
+    InitializePin(GPIOC, GPIO_PIN_0 | GPIO_PIN_1,GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0); // led lightbulb (green, red)
+    LiquidCrystal(GPIOB, GPIO_PIN_3, GPIO_PIN_0,GPIO_PIN_4, GPIO_PIN_10,GPIO_PIN_6,GPIO_PIN_9,GPIO_PIN_0); // Lcd display
 
-#ifdef TIME_RAND
-    // This illustrates the use of HAL_GetTick() to get the current time,
-    // plus the use of random() for random number generation.
-    
-    // Note that you must have "#include <stdlib.h>"" at the top of your main.c
-    // in order to use the srand() and random() functions.
-
-    // while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // wait for button press
-    // srand(HAL_GetTick());  // set the random seed to be the time in ms that it took to press the button
-    // if the line above is commented out, your program will get the same sequence of random numbers
-    // every time you run it (which may be useful in some cases)
-
-    while (true) // loop forever
-    {
-        while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // wait for button press
-
-        // Display the time in milliseconds along with a random number.
-        // We use the sprintf() function to put the formatted output into a buffer;
-        // see https://www.tutorialspoint.com/c_standard_library/c_function_sprintf.htm for more
-        // information about this function
-        char buff[100];
-        sprintf(buff, "Time: %lu ms   Random = %ld\r\n", HAL_GetTick(), random());
-        // lu == "long unsigned", ld = "long decimal", where "long" is 32 bit and "decimal" implies signed
-        SerialPuts(buff); // transmit the buffer to the host computer's serial monitor in VSCode/PlatformIO
-
-        while (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // wait for button to be released
-    }
-#endif
-
-#ifdef KEYPAD
-    // Read buttons on the keypad and display them on the console.
-
-    // this string contains the symbols on the external keypad
-    // (they may be different for different keypads)
-    char *keypad_symbols = "123A456B789C*0#D";
-    // note that they're numbered from left to right and top to bottom, like reading words on a page
-
-    InitializeKeypad();
-    while (true)
-    {
-        while (ReadKeypad() < 0);   // wait for a valid key
-        SerialPutc(keypad_symbols[ReadKeypad()]);  // look up its ASCII symbol and send it to the hsot
-        while (ReadKeypad() >= 0);  // wait until key is released
-    }
-#endif
-
-#ifdef KEYPAD_CONTROL
-    // Use top-right button on 4x4 keypad (typically 'A') to toggle LED.
-
-    InitializeKeypad();
-    while (true)
-    {
-        while (ReadKeypad() < 0);   // wait for a valid key
-        int key = ReadKeypad();
-        if (key == 3)  // top-right key in a 4x4 keypad, usually 'A'
-            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);   // toggle LED on or off
-         while (ReadKeypad() >= 0);  // wait until key is released
-    }
-#endif
-
-#ifdef SEVEN_SEGMENT
-    // Display the numbers 0 to 9 inclusive on the 7-segment display, pausing for a second between each one.
-    // (remember that the GND connection on the display must go through a 220 ohm current-limiting resistor!)
-    
-    Initialize7Segment();
-    while (true)
-        for (int i = 0; i < 10; ++i)
-        {
-            Display7Segment(i);
-            HAL_Delay(1000);  // 1000 milliseconds == 1 second
-        }
-#endif
-
-#ifdef KEYPAD_SEVEN_SEGMENT
-    // Combines the previous two examples, displaying numbers from the keypad on the 7-segment display.
-
-    // this string contains the symbols on the external keypad
-    // (they may be different for different keypads)
-    char *keypad_symbols = "123A456B789C*0#D";
-    // note that they're numbered from left to right and top to bottom, like reading words on a page
-
-    InitializeKeypad();
-    Initialize7Segment();
-    while (true)
-    {
-        int key = ReadKeypad();
-        if (key >= 0)
-            Display7Segment(keypad_symbols[key]-'0');  // tricky code to convert ASCII digit to a number
-    }
-#endif
-
-#ifdef COLOR_LED
-    // Cycle through all 8 possible colors (including off and white) as the on-board button is pressed.
-    // This example assumes that the color LED is connected to pins D11, D12 and D13.
-
-    // Remember that each of those three pins must go through a 220 ohm current-limiting resistor!
-    // Also remember that the longest pin on the LED should be hooked up to GND.
-
-    InitializePin(GPIOA, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);  // initialize color LED output pins
-    while (true) {
-        for (int color = 0; color < 8; ++color) {
-            // bottom three bits indicate which of the three LEDs should be on (eight possible combinations)
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, color & 0x01);  // blue  (hex 1 == 0001 binary)
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, color & 0x02);  // green (hex 2 == 0010 binary)
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, color & 0x04);  // red   (hex 4 == 0100 binary)
-
-            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));   // wait for button press 
-            while (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));  // wait for button release
-        }
-    }
-#endif
-
-#ifdef ROTARY_ENCODER
-    // Read values from the rotary encoder and update a count, which is displayed in the console.
-
-    InitializePin(GPIOB, GPIO_PIN_5, GPIO_MODE_INPUT, GPIO_PULLUP, 0);   // initialize CLK pin
-    InitializePin(GPIOB, GPIO_PIN_4, GPIO_MODE_INPUT, GPIO_PULLUP, 0);   // initialize DT pin
-    InitializePin(GPIOB, GPIO_PIN_10, GPIO_MODE_INPUT, GPIO_PULLUP, 0);  // initialize SW pin
-    
-    bool previousClk = false;  // needed by ReadEncoder() to store the previous state of the CLK pin
-    int count = 0;             // this gets incremented or decremented as we rotate the encoder
-
-    while (true)
-    {
-        int delta = ReadEncoder(GPIOB, GPIO_PIN_5, GPIOB, GPIO_PIN_4, &previousClk);  // update the count by -1, 0 or +1
-        if (delta != 0) {
-            count += delta;
-            char buff[100];
-            sprintf(buff, "%d  \r", count);
-            SerialPuts(buff);
-        }
-        bool sw = !HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10);  // read the push-switch on the encoder (active low, so we invert it using !)
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, sw);  // turn on LED when encoder switch is pushed in
-    }
-#endif
-
-#ifdef ANALOG
-    // Use the ADC (Analog to Digital Converter) to read voltage values from two pins.
-
-    __HAL_RCC_ADC1_CLK_ENABLE();        // enable ADC 1
-    ADC_HandleTypeDef adcInstance;      // this variable stores an instance of the ADC
-    InitializeADC(&adcInstance, ADC1);  // initialize the ADC instance
-    // Enables the input pins
-    // (on this board, pin A0 is connected to channel 0 of ADC1, and A1 is connected to channel 1 of ADC1)
-    InitializePin(GPIOA, GPIO_PIN_0 | GPIO_PIN_1, GPIO_MODE_ANALOG, GPIO_NOPULL, 0);   
-    while (true)
-    {
-        // read the ADC values (0 -> 0V, 2^12 -> 3.3V)
-        uint16_t raw0 = ReadADC(&adcInstance, ADC_CHANNEL_0);
-        uint16_t raw1 = ReadADC(&adcInstance, ADC_CHANNEL_1);
-
-        // print the ADC values
-        char buff[100];
-        sprintf(buff, "Channel0: %hu, Channel1: %hu\r\n", raw0, raw1);  // hu == "unsigned short" (16 bit)
-        SerialPuts(buff);
-    }
-#endif
-
-#ifdef PWM
-    // Use Pulse Width Modulation to fade the LED in and out.
-    uint16_t period = 100, prescale = 16;
+    // servo motor 
+    uint16_t period = 9999, prescale = 15; // period and prescale
 
     __TIM2_CLK_ENABLE();  // enable timer 2
     TIM_HandleTypeDef pwmTimerInstance;  // this variable stores an instance of the timer
     InitializePWMTimer(&pwmTimerInstance, TIM2, period, prescale);   // initialize the timer instance
-    InitializePWMChannel(&pwmTimerInstance, TIM_CHANNEL_1);          // initialize one channel (can use others for motors, etc)
+    InitializePWMChannel(&pwmTimerInstance, TIM_CHANNEL_2);          // initialize one channel (can use others for motors, etc)
+    InitializePin(GPIOB, GPIO_PIN_3, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_AF1_TIM2); // connect the motor to the timer output
 
-    InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_AF1_TIM2); // connect the LED to the timer output
-
-    while (true)
-    {
-        // fade the LED in by slowly increasing the duty cycle
-        for (uint32_t i = 0; i < period; ++i)
-        {
-            SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_1, i);
-            HAL_Delay(5);
-        }
-        // fade the LED out by slowly decreasing the duty cycle
-        for (uint32_t i = period; i > 0; --i)
-        {
-            SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_1, i);
-            HAL_Delay(5);
-        }
+    // lcd display show words 
+    print("Convert to Decimal:"); // first row 
+    setCursor(0,1); // second row 
+    print(b1);
+    print(" ");
+    print(b2);
+    print(" ");
+    print(b3);
+    print(" ");
+    print(b4);
+    
+    // scrolling through the boxes in the lcd display
+    for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
+        // scroll one position left:
+        scrollDisplayLeft();
+        // wait a bit:
+        HAL_Delay(350);
     }
-#endif
+    // scroll 29 positions (string length + display length) to the right
+
+    // to move it offscreen right:
+
+    for (int positionCounter = 0; positionCounter < 29; positionCounter++) {
+        // scroll one position right:
+        scrollDisplayRight();
+        // wait a bit:
+        HAL_Delay(350);
+    }
+
+    // scroll 16 positions (display length + string length) to the left
+
+    // to move it back to center:
+
+    for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+        // scroll one position left:
+        scrollDisplayLeft();
+        // wait a bit:
+        HAL_Delay(350);
+    }
+    
+    // declaring variables 
+    bool prvUp = 0; 
+    bool prvRight = 0;
+    bool prvButton = 0;
+    int deltaUp = 0; // if the joystick is pushed up 
+    int deltaRight = 0; // if joystick is pushed to the right 
+    int deltaButton = 0; // if the button is pressed
+    int count = 0; // counter
+    int pos = 0;  // position
+    int arr[4] = {0,0,0,0}; // 4 digit 7 segment array 
+    int time = 180000; // timer for 3 minutes
+
+    while (HAL_GetTick() < time){ // while the timer has not reached to 3 minutes
+
+    // joystick input 
+        deltaUp = InputNumberup(&prvUp);
+        deltaRight = InputNumberright(&prvRight);
+        deltaButton = buttonPushed(&prvButton);
+        
+        if (deltaUp != 0) {
+            arr[pos] += 1;
+            arr[pos] = arr[pos]%10; 
+        }
+
+        if (deltaRight != 0) {
+            pos += 1;
+            pos = pos%4;
+        } 
+        // get ccount from input and show on 7 segment display
+        count = arr[0]+arr[1]*10+arr[2]*100+arr[3]*1000;
+        Display4DigitInt7Segment(count); 
+
+        // if the input passcode is correct
+        if (count == result){
+            // green led lightbulb will turn on 
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, true);
+            HAL_Delay(250);  // 250 milliseconds == 1/4 second
+
+        // buzzer is turn on 
+            playTone(3,50);
+            playTone(2,100);
+
+        // servo motor is on 
+        for (uint32_t i = 0; i < period; ++i){
+        SetPWMDutyCycle(&pwmTimerInstance, TIM_CHANNEL_2, i);
+        HAL_Delay(1);
+        } 
+        // terminate the function
+        break;
+    }
+}
+    // if the timer is over
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, true); // red light is on 
+    HAL_Delay(250); 
+    clear(); // clear board
+    print("resetting");// print on first row in lcd display
+    main(); // restart the code 
     return 0;
 }
 
